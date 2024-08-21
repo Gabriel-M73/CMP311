1e05da42a449bc55232d48667c3b906b4d2dd0bb
Gabriel Medina <gabriel.medina@doane.edu>
2024-08-21 10:21:19 -0500

    check git with Alec 2 with new repos on hd with c:

---
+++ helloworld/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is cool!");
+
+Console.WriteLine("Enter Name");
+string username = Console.ReadLine();
+Console.WriteLine("Name is " + username);
+
+Console.WriteLine("Enter integer b//w 1 - 10");
+int num = Convert.ToInt32(Console.ReadLine());
+
+do
+if (num < 1 || num > 10)
+{
+    Console.WriteLine("Invalid integer, try again"); 
+} while (num < 1 || num > 10)
+    {
+    num = Convert.ToInt32(Console.ReadLine()); // int32.Parse also works
+}
